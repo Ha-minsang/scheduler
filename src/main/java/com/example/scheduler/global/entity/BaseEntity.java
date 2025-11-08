71f1793a89eba8c04a17459100212c24f9ab2ecb
Ha-minsang <gkalstkd@gmail.com>
2025-11-08 11:57:23 +0900

    LV.1 BasceEntity 생성시간/수정시간 추가

--- src/main/java/com/example/scheduler/global/entity/BaseEntity.java
+++ src/main/java/com/example/scheduler/global/entity/BaseEntity.java
@@ -1,4 +1,23 @@
 package com.example.scheduler.global.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+    // 생성 시간
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    // 수정 시간
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 }
