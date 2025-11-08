9d0ff0c1fa4ee75d24931738b9b69d95ac3c4bcf
Ha-minsang <gkalstkd@gmail.com>
2025-11-08 11:59:38 +0900

    LV.1 ScheduleRepository JpaRepository에 상속

--- src/main/java/com/example/scheduler/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/scheduler/schedule/repository/ScheduleRepository.java
@@ -1,4 +1,7 @@
 package com.example.scheduler.schedule.repository;
 
-public interface ScheduleRepository {
+import com.example.scheduler.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
