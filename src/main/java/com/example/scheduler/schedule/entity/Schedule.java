88430b66da57bbd8f487649ffc6b17836755ceae
Ha-minsang <gkalstkd@gmail.com>
2025-11-08 11:58:39 +0900

    LV.1 Schedule Entity 구성 요소 생성

---
+++ src/main/java/com/example/scheduler/schedule/entity/Schedule.java
@@ -0,0 +1,30 @@
+package com.example.scheduler.schedule.entity;
+
+import com.example.scheduler.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // schedule 작성자
+    @Column(nullable = false)
+    private String writer;
+
+    // schedule 제목
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    // schedule 내용
+    @Column(length = 200, nullable = false)
+    private String contents;
+}
